661ae65a76945c8cd4eb2d4c92ed9ef5e1c5b02a
td-suz00 <b0651118@gmail.com>
2019-05-04 17:02:25 +0900

    メッセージ送信機能
    masterにマージできなかったため再送

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   # has_many :comments
   has_many :group_users
   has_many :groups, through: :group_users
+  
   has_many :messages
 end
--- db/migrate/20190503042757_create_group_users.rb
+++ db/migrate/20190503042757_create_group_users.rb
@@ -3,7 +3,6 @@ class CreateGroupUsers < ActiveRecord::Migration[5.0]
     create_table :group_users do |t|
       t.references :group, foreign_key: true
       t.references :user,  foreign_key: true
-
       t.timestamps
     end
   end
