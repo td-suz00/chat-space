a8e86d842f6aad729d103247ea8481abcd5a40ce
td-suz00 <b0651118@gmail.com>
2019-05-05 13:00:49 +0900

    細かい部分の修正
    groupモデルクラスの中に、サブウインドウにグループの最新メッセージを表示できるようにメソッドの定義
    ビューからメソッドの呼び出し
    
    _form.scssの修正
    フラッシュメッセージの固定
    _main_headerの固定

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,14 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
+
+  def show_lase_message
+    # 最後のメッセージをlase_message変数に入れつつ、あるかどうかを判定
+    if (last_message = messages.last).present?
+      # 三項演算子　条件式 ? trueの時の値 : falseの時の値
+      last_message.text? ? last_message.text : "画像が投稿されています"
+    else
+      "まだメッセージはありません"
+    end
+  end
 end
