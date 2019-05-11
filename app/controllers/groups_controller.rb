26706935ebac6156d6b49452729d80227518f66c
td-suz00 <b0651118@gmail.com>
2019-05-11 20:03:55 +0900

    新規グループ作成　修正
    groupsコントローラの修正
    groups#new,edit,formビューファイルの修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,6 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
