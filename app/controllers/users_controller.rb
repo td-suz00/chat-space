b796d02ecbf964fea046ca571ca123cf9db220c3
td-suz00 <b0651118@gmail.com>
2019-05-11 20:47:28 +0900

    usersコントローラの修正
    where.notをnameからidに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "#{params[:keyword]}%").where.not(name: current_user.name)
-    
+    @users = User.where('name LIKE(?)', "#{params[:keyword]}%").where.not(id: current_user.id)
+
     respond_to do |format|
       format.html
       format.json
