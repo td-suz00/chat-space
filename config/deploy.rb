b8b1da2436cd072fb10ba9d100516baafca1f1e0
td-suz00 <b0651118@gmail.com>
2019-05-14 17:16:44 +0900

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, "git@github.com:td-suz00/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/secret_key_pair.pem']
